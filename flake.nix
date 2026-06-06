130fddbbbc18a483240469b4081e56f7d6eeba81
Immelancholy <lenalowes0@gmail.com>
2026-06-06 18:36:41 +0100

    chore: setup pre-commit and stuff

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    git-hooks.url = "github:cachix/git-hooks.nix";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,6 +18,7 @@
       ...
     }:
     let
+      inherit (self) inputs;
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -34,29 +36,25 @@
         };
     in
     {
+      formatter = forAllSystems (
+        pkgs:
+        let
+          system = pkgs.stdenv.hostPlatform.system;
+          config = self.checks.${system}.pre-commit-check.config;
+          inherit (config) package configFile;
+          script = ''
+            ${pkgs.lib.getExe package} run --all-files --config ${configFile}
+          '';
+        in
+        pkgs.writeShellScriptBin "pre-commit-run" script
+      );
+
       packages = forAllSystems (pkgs: {
         default = mkZarumet "build" pkgs;
         dev = mkZarumet "dev" pkgs;
         zarumet = self.packages.default;
       });
-      devShells = forAllSystems (
-        pkgs:
-        let
-          rustBin = rust-overlay.lib.mkRustBin { } pkgs;
-          rustToolchain = rustBin.fromRustupToolchainFile ./rust-toolchain.toml;
-        in
-        {
-          dev = pkgs.callPackage ./nix/shell.nix {
-            inherit rustToolchain;
-          };
-          direnv = pkgs.mkShell {
-            packages = [
-              self.packages.${pkgs.stdenv.hostPlatform.system}.dev
-            ];
-          };
-        }
-      );
-      formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
+
       overlays = {
         default = final: _prev: {
           zarumet = mkZarumet final;
@@ -64,6 +62,43 @@
         zarumet = self.overlays.default;
       };
 
-      homeModules.default = import ./nix/hm-module.nix self;
+      checks = forAllSystems (
+        pkgs:
+        let
+          system = pkgs.stdenv.hostPlatform.system;
+        in
+        {
+          pre-commit-check = inputs.git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixfmt.enable = true;
+            };
+          };
+        }
+      );
+
+      homeModules.default = import ./nix/hm.nix self;
+
+      devShells = forAllSystems (
+        pkgs:
+        let
+          rustBin = rust-overlay.lib.mkRustBin { } pkgs;
+          rustToolchain = rustBin.fromRustupToolchainFile ./rust-toolchain.toml;
+          system = pkgs.stdenv.hostPlatform.system;
+          inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+        in
+        {
+          default = pkgs.callPackage ./nix/shell.nix {
+            inherit rustToolchain;
+          };
+          direnv = pkgs.mkShell {
+            inherit shellHook;
+            buildInputs = enabledPackages;
+            packages = [
+              self.packages.${pkgs.stdenv.hostPlatform.system}.dev
+            ];
+          };
+        }
+      );
     };
 }
